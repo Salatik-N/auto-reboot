eb9a926172b131e8779c0b59a94900555809fc92
Mikita Salata <mikitasalata@MacBook-Air-Mikta.local>
2024-02-01 16:01:58 +0100

    Customize check

--- auto_reboot.sh
+++ auto_reboot.sh
@@ -1,28 +1,38 @@
 #!/bin/bash
 
+main_folder="$HOME/auto-reboot"
 log_file="$HOME/auto-reboot/log.txt"
 qubic_log_file="$HOME/auto-reboot/qubic_log.txt"
 screen_session="qubic"
 qubic="$HOME/projects/qubic/qliclient/qli-Client"
 
 if [ ! -f "$qubic" ]; then
+    echo "$(date '+%Y-%m-%d %H:%M:%S') No qli-Client file" | tee -a "$log_file"
     exit 1
 fi
 
+if [ ! -d "$main_folder" ]; then
+    echo "$(date '+%Y-%m-%d %H:%M:%S') Created main folder" | tee -a "$log_file"
+    mkdir -p "$main_folder"
+fi
+
 if [ ! -f "$log_file" ]; then
+    echo "$(date '+%Y-%m-%d %H:%M:%S') Created log.txt" | tee -a "$log_file"
     touch "$HOME/auto-reboot/log.txt"
 fi
 
 if [ ! -f "$qubic_log_file" ]; then
+    echo "$(date '+%Y-%m-%d %H:%M:%S') Created qubic_log.txt" | tee -a "$log_file"
     touch "$HOME/auto-reboot/qubic_log.txt"
 fi
 
 if ! crontab -l | grep -q "auto_reboot.sh"; then
+    echo "$(date '+%Y-%m-%d %H:%M:%S') Created cron" | tee -a "$log_file"
     echo "*/15 * * * * $HOME/auto-reboot/auto_reboot.sh" | crontab -
 fi
 
 if ! screen -ls | grep -q "$screen_session"; then
-   echo "$(date '+%Y-%m-%d %H:%M:%S') Create screen" | tee -a "$log_file"
+   echo "$(date '+%Y-%m-%d %H:%M:%S') Created screen" | tee -a "$log_file"
    screen -S "$screen_session" -d -m sh -c "$qubic >> $qubic_log_file"
 fi
 
